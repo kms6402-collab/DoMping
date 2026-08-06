9c4fd37d988c20aed747eaf17e2a36e1b32fdb6a
kms6402-collab <kms6402@gmail.com>
2026-08-06 16:21:51 +0900

    DoMping v1.9.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyFileVersion("1.9.0")]
-[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.1")]
+[assembly: AssemblyVersion("1.9.1.0")]
